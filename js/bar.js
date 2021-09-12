9aef0dd603abbf5c61e585bd94661cd264a7e71d
nyan3 <89848581+nyan3@users.noreply.github.com>
2021-09-12 18:43:24 +0800

    Add files via upload

---
+++ js/bar.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+/*恢复原始宽度0*/
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
---
+++ js/index.js
@@ -0,0 +1,12 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+if(document.domain!="nyan3.github.io")
+{
+ window.location.href="https://nyan3.github.io/";
+}
